ed54ff2391faedb5c95ec2506f42ec0be508f4ae
PraveenKumara2k33 <praveenkumara2k33@gmail.com>
2024-03-23 15:31:42 +0530

    first commit

--- src/Carousel.js
+++ src/Carousel.js
@@ -1,49 +1,62 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-import React from "react";
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import React from 'react';
 
 const Carousel = () => {
     const myStyle = {
         width:"100%",
         height:"700px"
-      };
-  return (
-  <div id="demo" className="carousel slide" data-bs-ride="carousel">
-    <div className="carousel-indicators">
-        <button type="button" data-bs-target="#demo" data-bs-slide-to="0" className="active"></button>
-        <button type="button" data-bs-target="#demo" data-bs-slide-to="1"></button>
-        <button type="button" data-bs-target="#demo" data-bs-slide-to="2"></button>
-    </div>
-    <div className="carousel-inner">
-        <div className="carousel-item active">
-            <img src="images/j1.jpg" className="d-block" style={myStyle} />
-            <div className="carousel-caption">
-                <h3>APk</h3>
-                <p>We had such a great Trainer</p>
+    };
+
+    // Define the images array
+    const images = [
+        {
+            src: "images/j1.jpg",
+            alt: "Slide 1",
+            caption: "APk",
+            description: "We had such a great Trainer"
+        },
+        {
+            src: "images/j2.jpg",
+            alt: "Slide 2",
+            caption: "Course",
+            description: "Structure of Fees Normal"
+        },
+        {
+            src: "images/j3.jpg",
+            alt: "Slide 3",
+            caption: "New York",
+            description: "We love the Big Apple!"
+        }
+    ];
+
+    return (
+        <div id="demo" className="carousel slide" data-bs-ride="carousel">
+            <div className="carousel-indicators">
+                {/* Map through the images array and generate buttons for each image */}
+                {images.map((images, index) => (
+                    <button key={index} type="button" data-bs-target="#demo" data-bs-slide-to={index} className={index === 0 ? "active" : ""}></button>
+                ))}
             </div>
+            <div className="carousel-inner">
+                {/* Map through the images array and generate carousel items for each image */}
+                {images.map((image, index) => (
+                    <div key={index} className={`carousel-item ${index === 0 ? "active" : ""}`}>
+                        <img src={image.src} className="d-block" style={myStyle} alt={image.alt} />
+                        <div className="carousel-caption">
+                            <h3>{image.caption}</h3>
+                            <p>{image.description}</p>
+                        </div>
+                    </div>
+                ))}
+            </div>
+            <button className="carousel-control-prev" type="button" data-bs-target="#demo" data-bs-slide="prev">
+                <span className="carousel-control-prev-icon"></span>
+            </button>
+            <button className="carousel-control-next" type="button" data-bs-target="#demo" data-bs-slide="next">
+                <span className="carousel-control-next-icon"></span>
+            </button>
         </div>
-    <div className="carousel-item">
-    <img src="images/j2.jpg" className="d-block" style={myStyle} />
-      <div className="carousel-caption">
-        <h3>Course</h3>
-        <p>Structure of Fees Normal</p>
-      </div> 
-    </div>
-    <div className="carousel-item">
-    <img src="images/j3.jpg" className="d-block" style={myStyle} />
-      <div className="carousel-caption">
-        <h3>New York</h3>
-        <p>We love the Big Apple!</p>
-      </div>  
-    </div>
-  </div>
-  <button className="carousel-control-prev" type="button" data-bs-target="#demo" data-bs-slide="prev">
-    <span className="carousel-control-prev-icon"></span>
-  </button>
-  <button className="carousel-control-next" type="button" data-bs-target="#demo" data-bs-slide="next">
-    <span className="carousel-control-next-icon"></span>
-  </button>
-</div>
-  )
+    );
 }
 
-export default Carousel
+export default Carousel;
--- src/Nav.js
+++ src/Nav.js
@@ -1,27 +1,31 @@
-import "bootstrap/dist/css/bootstrap.min.css";
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import React from "react";
+import App from "./App";
+import Couse from "./Couse";
+import TrainingCourse from "./TrainingCourse";
+import Footer from "./Footer";
 
 const Nav = () => {
   return (
 <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
   <div className="container-fluid">
-    <a className="navbar-brand">APK</a>
+    <a className="navbar-brand" href="/#">APK</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mynavbar">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="mynavbar">
       <ul className="navbar-nav me-auto">
         <li className="nav-item">
-          <a className="nav-link">HOME</a>
+          <a className="nav-link" href="/#app" onClick={App}>HOME</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link">COURSE</a>
+          <a className="nav-link"  href="/#Couse" onClick={Couse}>COURSE</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link">TRAINER</a>
+          <a className="nav-link" href="/#traine" onClick={TrainingCourse}>TRAINER</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link">REVIEW</a>
+          <a className="nav-link" href="/#footer"onClick={Footer}>REVIEW</a>
         </li>
       </ul>
       <form className="d-flex">
